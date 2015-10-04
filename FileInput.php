6dde5f51dca7a3dd68fa407a5ec546b1a1e88efb
armagedon007 <000yurik000@gmail.com>
2015-10-04 20:04:42 +0500

    Update FileInput.php

--- FileInput.php
+++ FileInput.php
@@ -66,7 +66,7 @@ class FileInput extends \kartik\base\InputWidget
         $input = $this->getInput('fileInput');
         $script = 'document.getElementById("' . $this->options['id'] . '").className.replace(/\bfile-loading\b/,"");';
         if ($this->showMessage) {
-            $validation = ArrayHelper::getValue($this->pluginOptions, 'showPreview', true) ? 'file preview and multiple file upload' : 'multiple file upload';
+            $validation = ArrayHelper::getValue($this->pluginOptions, 'showPreview', true) ? Yii::t('fileinput', 'file preview and multiple file upload') : Yii::t('fileinput', 'multiple file upload');
             $message = '<strong>' . Yii::t('fileinput', 'Note:') . '</strong> ' . Yii::t('fileinput', 'Your browser does not support {validation}. Try an alternative or more recent browser to access these features.', ['validation' => $validation]);
             $content = Html::tag('div', $message, $this->messageOptions) . "<script>{$script};</script>";
             $input .= "\n" . $this->validateIE($content);
